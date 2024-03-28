2d122fdcfb7f08b571b8fbb319bb7ff2e04c9afb
AnnaBilokon <Agnewka89@gmail.com>
2024-03-28 17:39:52 +0100

    added Cart Items component

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import CartItems from '../components/CartItems/CartItems'
 
 function Cart() {
-  return <div></div>
+  return (
+    <div>
+      <CartItems />
+    </div>
+  )
 }
 
 export default Cart
