0898e9dfb47d2381d2ac326b97922e81dfc3c57d
AnnaBilokon <Agnewka89@gmail.com>
2024-04-05 17:19:58 +0200

    added link to logo icon

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,17 +1,27 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import './Navbar.css'
 
 import logo from '../assets/logo_3.png'
 import cartIcon from '../assets/cart_icon.png'
 import { Link } from 'react-router-dom'
+import { ShopContext } from '../../context/ShopContext'
 
 const Navbar = () => {
   const [menu, setMenu] = useState('bookshop')
+  const { getTotalCartItems } = useContext(ShopContext)
 
   return (
     <div className="navbar">
-      <div className="nav_logo">
-        <img src={logo} alt="logo"></img>
+      <div
+        onClick={() => {
+          setMenu('logo')
+        }}
+        className="nav_logo"
+      >
+        <Link to="/">
+          <img src={logo} alt="logo"></img>
+        </Link>
+        {menu === 'logo' ? <hr /> : <></>}
       </div>
       <ul className="nav_menu">
         <li
@@ -72,7 +82,7 @@ const Navbar = () => {
           {' '}
           <img src={cartIcon} alt="cart_icon" />{' '}
         </Link>
-        <div className="nav_cart_count">0</div>
+        <div className="nav_cart_count">{getTotalCartItems()}</div>
       </div>
     </div>
   )
